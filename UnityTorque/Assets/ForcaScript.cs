b062f8e55b9f1d8e017eb71329ecce0c8fe0d775
Lucas Santos Rosa <luccasanttoz@gmail.com>
2016-07-10 17:00:18 -0300

    Teste 1

---
+++ UnityTorque/Assets/ForcaScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForcaScript : MonoBehaviour {
+
+	float F = 100f;
+	float B  = 0.05f;
+
+	/*
+	float angleX  = Mathf.Clamp(angleX, B, Bi);
+	float angleZ  = Mathf.Clamp(angleZ, Bi, B);
+
+	float Bi = (B) * (-1);
+	*/
+
+	public float Forca {
+		get{return F;}
+		set{F = value;}
+	}
+
+	public float Equilibrio{
+		get{return B;}
+		set{B = value;}
+	}
+}
+
+/*public float EQ1{
+		get{return angleX;}
+		set{angleX = value;}
+	}
+	public float EQ2{
+		get{return angleZ;}
+		set{angleZ = value;}
+	}
+
+	
+	public float E2{
+		get{return Bi;}
+		set{Bi = value;}
+	}*/
---
+++ UnityTorque/Assets/Movimento2.cs
@@ -0,0 +1,121 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movimento2 : MonoBehaviour {
+	public Rigidbody rb;
+	ForcaScript forca;
+
+	Vector3 Ti 	= new Vector3(0f, 0f, 1f);
+	Vector3 iT 	= new Vector3(1f, 0f, 0f);
+
+	public bool onNoff;
+
+	float F;
+
+	float angleX;
+	float angleZ;
+
+	float Xang;
+	float Zang;
+//	Vector3 Incl;
+
+	Vector3 x;
+	float theta;
+	Vector3 w;
+
+	Quaternion q;
+	Vector3 T;
+
+	// Use this for initialization
+	void Awake(){
+		rb = GetComponent<Rigidbody>();
+		forca = GetComponent<ForcaScript>();
+
+		x = Vector3.Cross(Ti.normalized, iT.normalized);
+		theta = Mathf.Asin(x.magnitude);
+		w = x.normalized * theta / Time.fixedDeltaTime;
+
+		q = transform.rotation * rb.inertiaTensorRotation;
+		T = q * Vector3.Scale(rb.inertiaTensor, (Quaternion.Inverse(q) * w));
+
+
+
+		angleX = 0;
+		angleZ = 0;
+
+		F = 100f;
+	}
+
+	void Start () {
+		//StartCoroutine ("Instab");
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Movimento();
+		InclinP ();
+		print (Zang);
+	}
+
+	void FixedUpdate()
+	{
+		rb.AddTorque (T * F, ForceMode.Acceleration);
+		//Inclinacao ();
+	}
+
+	void Movimento()
+	{
+		if (Input.GetAxis("Vertical") > 0)
+		{
+			rb.AddForce(Vector3.forward * 10);
+		}
+
+		if (Input.GetAxis("Vertical") < 0)
+		{
+			rb.AddForce(-Vector3.forward * 10);
+		}
+
+		if (Input.GetAxis("Horizontal") > 0)
+		{
+			rb.AddForce(Vector3.right * 10);
+		}
+
+		if (Input.GetAxis("Horizontal") < 0)
+		{
+			rb.AddForce(-Vector3.right * 10);
+		}
+	}
+
+	void InclinP()
+	{
+		//transform.eulerAngles = Incl;
+
+		if (onNoff == true) {
+			transform.eulerAngles = new Vector3 (angleX, transform.eulerAngles.y, angleZ);
+		} else {
+			Xang = transform.eulerAngles.x;
+			Zang = transform.eulerAngles.z;
+
+			InclinS (Xang, Zang);
+			//PararTorque (Xang, Zang);
+		}
+	}
+
+	void InclinS(float a, float b)
+	{
+		if (a != 0 && b != 0) {
+			angleX = a;
+			angleZ = b;
+		}
+	}
+
+	void PararTorque(float a, float b)
+	{
+		if((a > 52 && a < 53) || (b > 52 && b < 53))
+		{
+			F = 0f;
+
+		}
+	}
+}

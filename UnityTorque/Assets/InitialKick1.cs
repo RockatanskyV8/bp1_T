74d45f5db21af641403f977b7f757b226edec231
Lucas Santos Rosa <luccasanttoz@gmail.com>
2016-09-10 18:05:19 -0300

    Giro Estável
    são dois codigos, SpinRite e InitialKick

---
+++ UnityTorque/Assets/InitialKick1.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody))]
+public class InitialKick1 : MonoBehaviour {
+
+//	public Vector3 initialKick = new Vector3 (4f, 0, 0);
+	Vector3 Ti 	= new Vector3(0f, 0f, 1f);
+	Vector3 iT 	= new Vector3(1f, 0f, 0f);
+
+	private Rigidbody rb;
+	public float torqueTime;
+
+	Vector3 T;
+
+// Use this for initialization
+	void OnEnable () {
+		rb = GetComponent<Rigidbody> ();
+		Vector3 x = Vector3.Cross(Ti.normalized, iT.normalized);
+		float theta = Mathf.Asin(x.magnitude);
+		Vector3 w = x.normalized * theta / Time.fixedDeltaTime;
+
+		Quaternion q = transform.rotation * rb.inertiaTensorRotation;
+		T = q * Vector3.Scale(rb.inertiaTensor, (Quaternion.Inverse(q) * w));
+		rb.angularVelocity = T;
+	}
+}
---
+++ UnityTorque/Assets/ObjectPhysics.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectPhysics : MonoBehaviour 
+{
+	public Rigidbody rb;
+	public Rigidbody par;
+	Vector3 movement = new Vector3(0f, 0f, 0f);
+	Vector3 normal = new Vector3(0f, 0f, 0f);
+	float speed = 1000f;
+	float velocidade = 0f;
+	bool pulo = false;
+	bool isgrounded = false;
+
+
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.maxAngularVelocity = 1000;
+		rb.AddTorque(0, speed, 0);
+		StartCoroutine(PerdaVelocidade());
+	}
+
+	void Update () 
+	{
+		Controller();
+
+		//rb.AddTorque(-rb.angularVelocity * Time.deltaTime);
+
+		//rb.AddForce(n - p);
+		//transform.Rotate((n - p) * Time.deltaTime);
+
+		if (rb.angularVelocity.y > 1f)
+			transform.position += movement * 100 * Time.deltaTime;
+
+		if (pulo)
+		{
+			rb.velocity = rb.velocity + new Vector3(0, 150, 0) * Time.deltaTime;
+			pulo = false;
+		}
+
+		//movement = movement * Time.deltaTime;
+
+		Debug.DrawLine(Vector3.zero, normal, Color.red);
+	}
+
+	IEnumerator PerdaVelocidade()
+	{
+		while (true)
+		{
+			movement.x = movement.x * 0.9f;
+			movement.z = movement.z * 0.9f;
+			movement.y = movement.y * 0.9f;
+			yield return new WaitForSeconds(0.1f);
+		}
+	}
+
+	void Controller()
+	{
+		if (isgrounded)
+		{
+			if (Input.GetKey(KeyCode.RightArrow))
+			{
+				movement.x += 0.15f * Time.deltaTime;
+			}
+			if (Input.GetKey(KeyCode.LeftArrow))
+			{
+				movement.x += -0.15f * Time.deltaTime;
+			}
+			if (Input.GetKey(KeyCode.UpArrow))
+			{
+				movement.z += 0.15f * Time.deltaTime;
+			}
+			if (Input.GetKey(KeyCode.DownArrow))
+			{
+				movement.z += -0.15f * Time.deltaTime;
+			}
+			if (Input.GetKey(KeyCode.Space))
+			{
+				if (isgrounded)
+					pulo = true;
+			}
+
+			velocidade = Mathf.Pow(movement.x, 2) + Mathf.Pow(movement.z, 2);
+
+			if (Mathf.Sqrt(velocidade) > 1.0f)
+			{
+				movement.x = movement.x / velocidade;
+				movement.z = movement.z / velocidade;
+			}
+
+		}
+	}
+
+/*	void OnCollisionStay(Collision hit)
+	{
+		normal = hit.contacts[0].normal;        
+	}
+
+	void OnCollisionEnter(Collision hit)
+	{
+		if(hit.gameObject.tag == "floor")
+		{
+			isgrounded = true;
+		}
+	}
+
+	void OnCollisionExit(Collision hit)
+	{
+		if(hit.gameObject.tag == "floor")
+		{
+			isgrounded = false;
+		}
+	}*/
+}
+
